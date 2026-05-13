4ff84bb1912a7e6c072ce7e4505e6584860f3df1
Leon <lzueva@progeek.de>
2026-05-14 01:57:07 +0200

    add Englisch and Russian

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -2,6 +2,7 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import de_translation from "./de/translation.json"
 import en_translation from "./en/translation.json"
+import ru_translation from "./ru/translation.json"
 
 void i18next.use(initReactI18next).init({
     lng: "de",
@@ -12,6 +13,9 @@ void i18next.use(initReactI18next).init({
         },
         en: {
             translation: en_translation,
+        },
+        ru: {
+            translation: ru_translation,
         },
     },
 });
