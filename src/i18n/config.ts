2ee794ed03b92aed499c756fa8ba030cef44a636
Leon <lzueva@progeek.de>
2026-05-15 13:49:30 +0200

    remove Russian

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -2,7 +2,6 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import de_translation from "./de/translation.json"
 import en_translation from "./en/translation.json"
-import ru_translation from "./ru/translation.json"
 
 void i18next.use(initReactI18next).init({
     lng: "de",
@@ -13,10 +12,7 @@ void i18next.use(initReactI18next).init({
         },
         en: {
             translation: en_translation,
-        },
-        ru: {
-            translation: ru_translation,
-        },
+        }
     },
 });
 export default i18next;
